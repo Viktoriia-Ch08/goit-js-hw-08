81b6fed591903077e54b54a14d2cddd46cfb6fb9
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2023-07-29 11:52:40 +0300

    added function to save current time in player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const LS_KEY = 'videoplayer-current-time';
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem(LS_KEY, data.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem(LS_KEY) || 0);
