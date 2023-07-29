98ef7d43aa9686b06f38fec4c9857beadd95f215
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2023-07-29 12:23:48 +0300

    added functions for saving/deleting info in storage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const KEY = 'feedback-form-state';
+let formInfo = {};
+
+formEl.addEventListener('input', throttle(setStorageItem, 500));
+formEl.addEventListener('submit', clearInfo);
+
+function setStorageItem(event) {
+  formInfo[event.target.name] = event.target.value.trim();
+
+  const formInfoJSON = JSON.stringify(formInfo);
+  localStorage.setItem(KEY, formInfoJSON);
+}
+
+const onLoad = () => {
+  try {
+    const data = localStorage.getItem(KEY);
+    if (!data) return;
+    formInfo = JSON.parse(data);
+    Object.entries(formInfo).forEach(([key, value]) => {
+      formEl.elements[key].value = value;
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+window.addEventListener('load', onLoad);
+
+function clearInfo(event) {
+  event.preventDefault();
+  console.log(formInfo);
+  formInfo = {};
+  localStorage.removeItem(KEY);
+  formEl.reset();
+}
