50e146b3298fa105b24a23fed31f3d77cb0dfd5a
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2023-07-29 10:46:46 +0300

    added gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const listEl = document.querySelector('.gallery');
+
+const createImageItems = galleryItems => {
+  return galleryItems
+    .map(
+      img =>
+        `<li class="gallery__item">
+   <a class="gallery__link" href="${img.original}">
+      <img class="gallery__image" src="${img.preview}" alt="${img.description}"/>
+   </a>
+</li>`
+    )
+    .join('');
+};
+listEl.innerHTML = createImageItems(galleryItems);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
